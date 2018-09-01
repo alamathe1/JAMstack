31aad65535581ffb06471f9670ffc6b5f0a35155
Alan Mathew <alanmathew671@gmail.com>
2018-08-31 21:25:53 -0700

    github fcc hackacthon

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Now let's go build something great.</p>
+    <p>My name is Alan. I like to code for fun!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
